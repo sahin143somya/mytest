4c570985adba03f319ecd528e0338b3c7e17a441
sahin143somya <testcopy.in@gmail.com>
2025-05-17 16:37:43 +0530

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,9 +1,9 @@
 <script>
 alert("hello sona");
-<<<<<<< HEAD
+
 alert("hello pty");
-=======
+
 alert("hello my");
->>>>>>> d00d917090bc56937a3c719f032f396aea434082
+
 document.write("sunil");
 </script>
